d258c7b801c3c156dd191cc32bbf11a3e9010ede
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-09-23 19:26:58 -0300

    Adionar código base

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,17 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+      ),
+      body: Center(
+        child: Text('Texto'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
